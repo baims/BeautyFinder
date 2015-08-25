00bed5cdf73ed91a42d617d619b2ed0864218e31
Yousef Alhusaini <Yousef@Yousefs-MacBook-Pro.local>
2015-08-25 04:40:10 +0100

    Initial Commit

---
+++ BeautyFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeautyFinder
+//
+//  Created by Yousef Alhusaini on 8/25/15.
+//  Copyright (c) 2015 Yousef Alhusaini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
